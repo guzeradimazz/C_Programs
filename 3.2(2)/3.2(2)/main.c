356c329c5bf0cf213b148485be4b6380b4e73ac5
Dima <dguzerchuk@mail.ru>
2021-03-22 21:55:39 +0300

    lab 3 added

---
+++ 3.2(2)/3.2(2)/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  3.2(2)
+//
+//  Created by Дима Гузерчук on 12.03.21.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
